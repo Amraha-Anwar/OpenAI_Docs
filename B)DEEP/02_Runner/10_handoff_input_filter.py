9200f51e5cf318667d000347c21f185a493dec37
Amraha-Anwar <amrahaanwar@gmail.com>
2025-09-15 19:13:04 +0500

    handoff input filters

--- B)DEEP/02_Runner/10_handoff_input_filter.py
+++ B)DEEP/02_Runner/10_handoff_input_filter.py
@@ -1,52 +1,48 @@
-from agents import (
-        Agent,
-        Runner,
-        handoff,
-        HandoffInputData,
-        function_tool
-)
+from agents import Agent, Runner, function_tool, handoff, HandoffInputData
 from dotenv import load_dotenv
-from rich import print
-import asyncio
 
 load_dotenv()
 
-def summarize_billing_issues(data: HandoffInputData) -> HandoffInputData:
-    print("\n\n[HANDOFF] Transferring to technical support...\n\n")
-    summarized_issue = "User has a technical issue with their account."
-    print("\nSummarized_issue:", data.input_history)
-    print("\n\n[ITEM 2]", data.pre_handoff_items)
-    print("\n\n[ITEM 1]", data.new_items)
+def summarized_user_input(data: HandoffInputData) -> HandoffInputData:
+    print("\n---Handing off to Spanish Agent---\n")
+    summarize_input = "User wants the text to be tranlated into Spanish."
 
+    print(f"\n[ITEM 1]: {data.input_history}")
+    print(f"\n[ITEM 2]: {data.pre_handoff_items}")
+    print(f"\n[ITEM 3]: {data.new_items}")
 
     return HandoffInputData(
-        input_history=summarized_issue,
-        pre_handoff_items=(),  
-        new_items=(),         
-)
+        input_history=summarize_input,
+        pre_handoff_items=(),
+        new_items=(),
+    )
+
 
 @function_tool
-async def get_user_account_status(user_id: str) -> str:
-    """Gets the account status for a user."""
-    return f"Account status for user {user_id} is active."
+def add(a:int, b:int)-> str:
+    "Returns the sum of 2 numbers"
+    return f"The sum of {a} and {b} is = {a + b}."
 
-technical_support_agent: Agent = Agent(
-    name="TechnicalSupportAgent",
-    instructions="You are a technical support expert. You troubleshoot account and service issues for users.",
-    model='gpt-4o-mini',
-    tools=[get_user_account_status],
+
+spanish_agent = Agent(
+    name = "Spanish Speaking Agent",
+    instructions = "You are a Spanish Agent. You translate user's given text into spanish",
+    model = 'gpt-4o-mini'
 )
 
-billing_agent: Agent = Agent(
-    name="BillingAgent",
-    instructions="You are a billing expert. You handle all payment and subscription questions. For any technical issues, you must hand off to the TechnicalSupportAgent.",
-    model='gpt-4o-mini',
-    tools=[get_user_account_status],
-    handoffs=[handoff(agent=technical_support_agent, input_filter=summarize_billing_issues)]
+math_agent = Agent(
+    name = "Math Expert",
+    instructions= "You are the expert of maths. Solve user's math related query by must using the tool [add] if needed."
+    "For Spanish related queries let spanish agent handle the task",
+    model = 'gpt-4o-mini',
+    tools = [add],
+    handoffs = [handoff(agent=spanish_agent, input_filter=summarized_user_input)]
 )
 
-initial_query = "My subscription payment went through, but my account is still showing as inactive."
-result = Runner.run_sync(billing_agent, initial_query)
+result = Runner.run_sync(
+    math_agent,
+    "translate 'have a nice day' into spanish."
+)
 
-print("\nAGENT NAME", result.last_agent.name)
-print("\n[FINAL RESPONSE]", result.final_output)
+print(f"Last Agent: '{result.last_agent.name}'\n")
+print(f"Final Output: '{result.final_output}'\n")
